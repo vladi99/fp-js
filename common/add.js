1c2e0d053f77bc54d84412bccd935a31baf88ffe
vladi99 <vlpetrovic99@gmail.com>
2019-11-23 16:40:07 +0000

    implement add function

--- common/add.js
+++ common/add.js
@@ -1,5 +1,5 @@
 function add() {
-
+  return [...arguments].reduce((acc, el) => acc + el);
 }
 
 module.exports = add;
--- test/equal.js
+++ test/equal.js
@@ -2,7 +2,7 @@ function equal(actual, expected) {
   if (JSON.stringify(expected) === JSON.stringify(actual)) {
     console.log('\x1b[32m%s\x1b[0m', 'Pass');
   } else {
-    console.log('\x1b[31m%s\x1b[0m', `Fail: `, { actual, expected });
+    console.log('\x1b[31m%s\x1b[0m', `Fail: `);
   }
 }
 

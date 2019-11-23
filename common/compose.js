3166bab8eb253940e1e3ae0b82ef102c778665ba
vladi99 <vlpetrovic99@gmail.com>
2019-11-23 17:11:20 +0000

    implement compose function

--- common/compose.js
+++ common/compose.js
@@ -1,5 +1,5 @@
-function compose() {
-  return () => {}
+function compose(...functions) {
+  return functions.reduceRight((acc, func) => (...args) => func(acc(...args)));
 }
 
 module.exports = compose;

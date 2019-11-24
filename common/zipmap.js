a4a3424a4aac4db02c62268c87326d047de0c6da
vladi99 <vlpetrovic99@gmail.com>
2019-11-24 01:56:32 +0000

    Implement zipmap function

--- common/zipmap.js
+++ common/zipmap.js
@@ -1,5 +1,5 @@
-function zipmap() {
-
+function zipmap(...arrays) {
+  return arrays[0].reduce((accObject, key, index) => ({...accObject, [key]: arrays[1][index]}), {});
 }
 
 module.exports = zipmap;

028df8ed20b4b18de5836237fc00a81b983c8ed4
vladi99 <vlpetrovic99@gmail.com>
2019-12-20 22:42:44 +0000

    implement loose curry

---
+++ common/looseCurry.js
@@ -0,0 +1,11 @@
+function looseCurry(fn, arity = fn.length) {
+  return (function nextCurried(prevArgs) {
+    return function curried(...nextArg) {
+      const args = [...prevArgs, ...nextArg];
+
+      return args.length >= arity ? fn(...args) : nextCurried(args);
+    }
+  })([])
+}
+
+export default looseCurry;
--- tests/test.js
+++ tests/test.js
@@ -40,6 +40,7 @@ import postwalk from '../common/postwalk';
 import prewalk from '../common/prewalk';
 import wrapUnlessZero from '../common/wrapUnlessZero';
 import curry from '../common/curry'
+import looseCurry from '../common/looseCurry'
 
 function test() {
   equal(clist(1, 2, 3), [1, 2, 3]);
@@ -69,6 +70,7 @@ function test() {
   equal(partial(sub, 10)(1, 2), 7);
 
   equal(curry(add, 4)(1)(2)(3)(4), 10);
+  equal(looseCurry(add, 5)(1)(2, 3)(4, 5), 15);
 
   equal(transpose([[1, 2, 3], [4, 5, 6]]), [[1, 4], [2, 5], [3, 6]]);
 
